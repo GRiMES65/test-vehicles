bf4fe077d71c4beb913ad9194ee5cf861b84e4eb
GRiMES65 <129760861+GRiMES65@users.noreply.github.com>
2025-02-10 21:27:42 +0000

    Initial Commit

---
+++ Vehicles.java
@@ -0,0 +1,18 @@
+public class Vehicles {
+    private String make;
+    private String fuel;
+    private String color;
+
+    public Vehicles (String make, String fuel, String color){
+        this.make = make;
+        this.fuel = fuel;
+        this.color = color;
+    }
+}
+
+public String getMake(){
+    return make;
+}
+public void setMake(String make){
+    
+}
